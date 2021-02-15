d035bdfe76dbde34249456a07aec36c2a6864f43
Anderson Jorge <anderson.jorge33@gmail.com>
2021-02-15 12:43:51 -0300

    arquivo da aula sobre loop wordpress

--- wp-content/themes/curso/index.php
+++ wp-content/themes/curso/index.php
@@ -20,16 +20,24 @@
                         //se houver algum post
                         if( have_posts() ):
                             //enquanto houver post, mostre-os para gente
-                            while( have_posts() ): the_post ();
-
+                            while( have_posts() ): the_post();
                         ?>
+                        <article>
+                            <h2><?php the_title(); ?></h2>
+                            <p>published in <?php echo get_the_date(); ?> by <?php the_author_posts_link(); ?> </p>
+                            <p>Categories: <?php the_category( ' ' ); ?> </p>
+                            <p><?php the_tags( 'Tags:', ', ' );?></p>
+                            <?php the_content(); ?>
+                        </article>
                         <?php
                             endwhile;
                         else:
-
                         ?>
+
                         <p>Theme's nothing yet to be displayed... </p>
+
                         <?php endif; ?>
+
                     </div>
                 </div>
             </div>
