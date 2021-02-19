0e91bfed55fb1e3ec550cc10961cfddc9ec7ae7e
Anderson Jorge <anderson.jorge33@gmail.com>
2021-02-19 19:32:57 -0300

    arquivo da aula criação de arquivos de páginas

---
+++ wp-content/themes/curso/template-parts/content-archive.php
@@ -0,0 +1,13 @@
+<article <?php post_class(); ?>>
+    <h2>
+        <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+    </h2>
+    <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(array(275, 275)); ?></a>
+    <div class="meta-info">
+        <p>published in <?php echo get_the_date(); ?> by <?php the_author_posts_link(); ?> </p>
+        <p><?php the_tags('Tags: ', ', '); ?></p>
+    </div>
+
+    <!-- conteúdo resumido do post -->
+    <?php the_excerpt(); ?>
+</article>
