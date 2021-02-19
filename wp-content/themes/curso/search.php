bbef2727b21d3dd6ffcc24ed7d0dce9e875f2129
Anderson Jorge <anderson.jorge33@gmail.com>
2021-02-19 18:32:03 -0300

    arquivo da aula sobre paginação de blogs

--- wp-content/themes/curso/search.php
+++ wp-content/themes/curso/search.php
@@ -16,6 +16,14 @@
                 endif;
             endwhile;
 
+            the_posts_pagination(
+                array(
+                    'prev_text' => 'Previous',
+                    'next_text' => 'Next',
+                    'screen_reader_text' => true
+                )
+            );
+
             ?>
         </div>
     </div>
